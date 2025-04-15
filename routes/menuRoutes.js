f41b9aaf160565967b80e34a05c888e313579421
SimonGebru <te.gebru@hotmail.se>
2025-04-15 19:16:33 +0200

    fixat middleware

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const menuController = require('../controllers/menuController');
+const validateMenuInput = require('../middleware/validateMenuInput'); 
 
 router.get('/', menuController.getMenu);
-router.post('/', menuController.addProduct);
+router.post('/', validateMenuInput, menuController.addProduct); 
 
 module.exports = router;
