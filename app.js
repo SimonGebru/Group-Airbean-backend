e80cf8874e4f9f3d966dc27b6e154091165fcbb3
SimonGebru <te.gebru@hotmail.se>
2025-04-21 15:39:01 +0200

    hej

--- app.js
+++ app.js
@@ -3,27 +3,29 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const menuRoutes = require('./routes/menuRoutes');
 const authRoutes = require('./routes/authRoutes');
-const orderRoutes = require('./routes/orderRoutes'); // 🆕
+const orderRoutes = require('./routes/orderRoutes'); 
+const orderHistoryRoutes = require('./routes/orderHistoryRoutes');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+
 app.use(express.json());
 
-// MongoDB-koppling
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB ansluten'))
-  .catch(err => console.error('❌ MongoDB-fel:', err));
+  .catch(err => console.error(' MongoDB-fel:', err));
 
-// Routes
+
 app.use('/api/menu', menuRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/orders', orderRoutes); // 🧩 Här kopplas order ihop
+app.use('/api/orders', orderRoutes); 
+app.use('/api/orders/history', orderHistoryRoutes);
 
-// Starta servern
+
 app.listen(PORT, () => {
   console.log(`🚀 Servern körs på http://localhost:${PORT}`);
 });
